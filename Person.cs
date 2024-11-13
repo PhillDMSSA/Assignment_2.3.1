f2512962fb39c292242aa9de13270d95eb425068
phillip deleon <phillipdeleon68@gmail.com>
2024-11-13 16:34:39 -0600

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._3._1
+{
+    internal class Person
+    {
+        public Person(string firstname, string lastname, int age, string address)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+            Address = address;
+        }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+
+namespace Assignment_2._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a variable for the text file
+            string filePath = @"C:\Users\phillipdeleon\Documents\Text.txt";
+            // saves entire list of text file inside array
+            //string[] lines = File.ReadAllLines(filePath); 
+            //define new list of type string
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList(); //Converts an array to list
+
+            foreach (string line in lines)  //outputs the text from textfile
+            {
+                Console.WriteLine(line);
+            }
+
+            lines.Add("Phillip Deleon 27 935 Bel Aire"); //adds text to text file
+            File.WriteAllLines(filePath, lines); //saves written input to text file
+
+
+
+        }
+    }
+}
